71c208794e31ab0b8c68c2c4b21bd78e0bcede24
santsane <junjunsanji@gmail.com>
2026-02-09 20:18:46 +0500

    chore: setup application.properties and the main class

---
+++ src/main/java/com/jrs/job_recruitment_system/JobRecruitmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jrs.job_recruitment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRecruitmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRecruitmentSystemApplication.class, args);
+	}
+
+}
